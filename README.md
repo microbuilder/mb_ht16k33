7ab7229e3e1fc6338211a47f4f4a594e7c8a6271
Kevin Townsend <contact@microbuilder.eu>
2018-09-16 02:40:25 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# mb_htk16k33
-Driver for the HTK16K33 14-segment alphanumeric display controller for Apache Mynewt
+# HTK16K33 14-Segment Display Driver for Apache Mynewt
+
+The main package documentation for this repository is available at
+[microbuilder.io/mb_htk16k33](http://microbuilder.io/mb_htk16k33).
+
+# Folder Structure
+
+- **Unit tests** are provided in the `test` folder, which can also
+serve as an example of how the various functions and structs in this package
+can be used.
+
+- Reasonably complete **documentation** is available in the `docs` folder.
---
+++ include/mb_htk16k33/mb_htk16k33.h
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * \defgroup HTK16K33 htk15k33
+ *
+ * Functions to work with the HT16K33 14-segment display driver.
+ */
+
+ #ifndef _MB_HTK16K33_H_
+ #define _MB_HRK16K33_H_
+
+ #include <stdint.h>
+ #include "os/mynewt.h"
+
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+
+ /**
+  * @addtogroup STRUCTS Structs and Enums
+  *
+  * HTK16K33-related structs and enums.
+  *
+  * \ingroup HTK16K33
+  *  @{ */
+
+/** @} */ /* End of STRUCT group */
+
+/**
+ * @addtogroup FUNC Functions
+ *
+ * Functions to work with the HTK16K33.
+ *
+ * \ingroup HTK16K33
+ *  @{ */
+
+/** @} */ /* End of FUNC group */
+
+#if MYNEWT_VAL(HTK16K33_CLI)
+int htk16k33_shell_init(void);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _MB_HTK16K33_H_ */
+
+/** @} */ /* End of HTK16K33 group */
